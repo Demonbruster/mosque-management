cdd286c492ebdabf884fe21db6db2144bb8d37ae
A Aashik Ahamed <ahamedaashik071@gmail.com>
2026-03-31 12:31:40 +0530

    Added useForm

--- frontend/src/components/profile-update/userRequest.tsx
+++ frontend/src/components/profile-update/userRequest.tsx
@@ -1,22 +1,27 @@
 import { Button, Checkbox, Grid, Group, Select, TextInput } from '@mantine/core';
 import React, { useState } from 'react';
 import { genderEnum, personCategoryEnum } from '../../../../backend/src/db/schema';
-
-const userData = {
-  firstName: 'John',
-  lastName: 'Doe',
-  email: '[EMAIL_ADDRESS]',
-  phoneNumber: '1234567890',
-  nationalId: '1234567890',
-  notes: 'Notes',
-  gender: 'male',
-  category: 'Member',
-  whatsappOptIn: true,
-  isActive: true,
-};
+import { existingUser } from '../../../mockdata/userRequests';
+import { useForm } from '@mantine/form';
 
 export const UserDetailsChangeRequest = () => {
   const [changeRequest, setChangeRequest] = useState(false);
+  const [userData, setUserData] = useState(existingUser);
+
+  const form = useForm({
+    initialValues: {
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      email: userData.email,
+      phoneNumber: userData.phoneNumber,
+      gender: userData.gender,
+      category: userData.category,
+      nationalId: userData.nationalId,
+      notes: userData.notes,
+      whatsappOptIn: userData.whatsappOptIn,
+      isActive: userData.isActive,
+    },
+  });
 
   const genderOptions = genderEnum.enumValues.map((gender) => ({
     value: gender,
@@ -130,22 +135,62 @@ export const UserDetailsChangeRequest = () => {
             </Grid.Col>
 
             <Grid.Col span={6}>
-              <TextInput label="First Name" placeholder="First Name" />
-              <TextInput label="Last Name" placeholder="Last Name" />
-              <TextInput label="Email" placeholder="Email" />
-              <TextInput label="Phone Number" placeholder="Phone Number" />
-              <Select label="Gender" placeholder="Gender" data={genderOptions} />
-              <Select label="Category" placeholder="Category" data={personCategoryOptions} />
-              <TextInput label="National ID" placeholder="National ID" />
-              <TextInput label="Notes" placeholder="Notes" />
-              <Group mt={'sm'}>
-                <Checkbox label="WhatsApp Opt In" defaultChecked={userData.whatsappOptIn} />
-                <Checkbox label="Is Active" defaultChecked={userData.isActive} />
-              </Group>
+              <form
+                onSubmit={form.onSubmit((values) => {
+                  console.log(values);
+                })}
+              >
+                <TextInput
+                  label="First Name"
+                  placeholder="First Name"
+                  {...form.getInputProps('firstName')}
+                />
+                <TextInput
+                  label="Last Name"
+                  placeholder="Last Name"
+                  {...form.getInputProps('lastName')}
+                />
+                <TextInput label="Email" placeholder="Email" {...form.getInputProps('email')} />
+                <TextInput
+                  label="Phone Number"
+                  placeholder="Phone Number"
+                  {...form.getInputProps('phoneNumber')}
+                />
+                <Select
+                  label="Gender"
+                  placeholder="Gender"
+                  data={genderOptions}
+                  {...form.getInputProps('gender')}
+                />
+                <Select
+                  label="Category"
+                  placeholder="Category"
+                  data={personCategoryOptions}
+                  {...form.getInputProps('category')}
+                />
+                <TextInput
+                  label="National ID"
+                  placeholder="National ID"
+                  {...form.getInputProps('nationalId')}
+                />
+                <TextInput label="Notes" placeholder="Notes" {...form.getInputProps('notes')} />
+                <Group mt={'sm'}>
+                  <Checkbox
+                    label="WhatsApp Opt In"
+                    defaultChecked={userData.whatsappOptIn}
+                    {...form.getInputProps('whatsappOptIn')}
+                  />
+                  <Checkbox
+                    label="Is Active"
+                    defaultChecked={userData.isActive}
+                    {...form.getInputProps('isActive')}
+                  />
+                </Group>
+                <Grid justify="flex-end" mt="md">
+                  <Button type="submit">Request</Button>
+                </Grid>
+              </form>
             </Grid.Col>
-          </Grid>
-          <Grid justify="flex-end" mt="md">
-            <Button>Request</Button>
           </Grid>
         </>
       )}
